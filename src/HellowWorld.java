f7313b663ba9560ebd45dbd979ddf29e898d99f2
g12951te <g12951te@s30438.tsuda.ac.jp>
2014-04-11 11:15:29 +0900

    実行するとHellowWorldと表示するプログラムです。

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -9,6 +9,6 @@
  */
 public class HellowWorld {
     public static void main(String[] args) {
-        System.out.println("Hellow, world!");
+        System.out.println("Hellow, world!!!!");
     }
 }
