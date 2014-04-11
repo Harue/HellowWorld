13951668748925868e22278a8fc728e18c891ae7
g12951te <g12951te@s30438.tsuda.ac.jp>
2014-04-11 11:03:54 +0900

    実行するとHellowWorldと表示するプログラムです。

---
+++ src/HellowWorld.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author g12951te
+ */
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hellow, world!");
+    }
+}
